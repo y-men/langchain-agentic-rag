17c07e6f8fd61ca5f62183a2dfbc20f2737abc66
y-men <yaniv.menuhin@gmail.com>
2025-08-07 09:52:40 +0300

    Backup: Initial commit of agentic RAG implementation (without secrets)

---
+++ main.py
@@ -0,0 +1,12 @@
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+if __name__ == "__main__":
+    print("Hello from langchain-agentic-rag!")
+    # print("Loading from .mv properties:")
+    # for k, v in os.environ.items():
+    #     print(f"{k}={v}")
